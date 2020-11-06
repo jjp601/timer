750bc2298eaeef802dea7985dcea1086034b5151
Jon Powell <jpowell68601@gmail.com>
2020-11-05 21:36:29 -0600

    Setup getter and setter for time remaining

--- index.js
+++ index.js
@@ -5,10 +5,32 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start)
+        this.pauseButton.addEventListener('click', this.pause)
     }
 
-    start() {
-        console.log('Time to start the timer!');
+    start = () => {
+        this.tick()
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+        }
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value)
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+
+    pause = () => {
+        clearInterval(this.interval)
     }
 }
 
